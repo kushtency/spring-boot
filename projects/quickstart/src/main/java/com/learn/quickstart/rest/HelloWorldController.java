c4b1284a2adda9553a833a45d5a4a493284a993e
kushtency <kushagra.strive@gmail.com>
2023-05-04 10:55:04 +0530

    hello world program

---
+++ projects/quickstart/src/main/java/com/learn/quickstart/rest/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.learn.quickstart.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String hello(){
+        return "hello world";
+    }
+}
